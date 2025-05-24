da28c9aa7c688a9a8f78f1ea4561876a37044395
Amadeu Marques <maramadeu@gmail.com>
2025-05-24 10:38:26 +0100

    remove import

--- src/stp/mod.rs
+++ src/stp/mod.rs
@@ -12,7 +12,6 @@ use futures::future::{ok, select};
 use konst::string::split;
 use regex::Replacer;
 use scoped_threadpool::Pool;
-use std::clone::CloneToUninit;
 use std::collections::BTreeMap;
 use std::fmt::{Debug, Formatter};
 use std::sync::{Arc, Mutex, RwLock};
